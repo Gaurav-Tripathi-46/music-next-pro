36c9e8cb315547012f0354a47f966be4c15074fb
Gaurav-Tripathi-46 <gauravtripathi0026@gmai.com>
2025-07-15 18:43:10 +0530

    completed hero page

--- musicnext/src/app/page.tsx
+++ musicnext/src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
-    < >
-    <h1 className=" text-center text-yellow-300 ">hello</h1>
-    </> 
+    <main className="min-h-screen bg-black/[.96] antialiased bg-grid-white/[0.2]">
+    <HeroSection/>
+    </main> 
   );
 }
